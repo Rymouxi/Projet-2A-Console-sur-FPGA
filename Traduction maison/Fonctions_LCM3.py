256b3e620285af86f8ed31c7b0b5fa626c24fd57
Nyries <145461178+Nyries@users.noreply.github.com>
2023-10-10 17:08:35 +0200

    Update Fonctions_LCM3.py

--- Traduction maison/Fonctions_LCM3.py
+++ Traduction maison/Fonctions_LCM3.py
@@ -1,17 +1,17 @@
 from DecToBin import *
-from add_sub import *
+from add_sub_mov import *
 from Register_recognition import *
 
 #On travaille toujours avec des chaînes de caractères
 #Les fonctions appelées renvoient des chaînes de caractère
 
-def reconnaissance_instruction(instruction):
+def recognition_instruction(instruction):
     n=len(instruction)
     l=''
     if instruction[0:3]=='ADD':
-        add(instruction)
+        l=ADD(instruction)
     elif instruction[0:3]=='AND ':
-        l=and_(instruction)
+        l=AND_(instruction)
     elif instruction[0]=='B':
         b_instruct(instruction)
     elif instruction[0:3]=='CMP':
@@ -23,17 +23,17 @@ def reconnaissance_instruction(instruction):
     elif instruction[0:3]=='LSL':
         lsl(instruction)
     elif instruction[0:3]=='MOV':
-        mov(instruction)
+        l=MOV(instruction)
     elif instruction[0:3]=='STR':
         str_(instruction)
     elif instruction[0:3]=='SUB':
-        sub(instruction)
+        l=SUB(instruction)
     else:
         print("Error Syntax")
     return l
 
 
-def and_(instruction:str):
+def AND_(instruction:str):
     """Traduction de l'instruction and en langage machine de 0 et de 1\n
     L'instruction qu'elle renvoie est de type str"""
     #AND Rd,Rm
