ac9dd81ac399e60334d2ff0d9097172526172e3e
Nyries <145461178+Nyries@users.noreply.github.com>
2023-10-10 13:52:54 +0000

    Pending changes exported from your codespace

--- Traduction maison/add_sub.py
+++ Traduction maison/add_sub.py
@@ -1,32 +1,20 @@
 from DecToBin import*
+from Register_recognition import*
 
 def add(instruction):
-    #Partons du principe que l'instruction ne prend qu'un certain type de forme
-    #Nous avons donc: ADD Rd,Rn,#imm3
-    #ADD Rd,#imm8
-    #ADD Rd,Rn,Rm
+
     n=len(instruction)
+    l=''
+    register=Register_recognition(instruction)
+    Rlen=len(register)
 
-    if copie.count(',')==1:
-    #ADD Rd,#imm8
-        add_rd_imm8(instruction)
-
-    elif copie.count(',')==2:
-        if copie.count('#')==0:
-        #ADD Rd,Rn,Rm
-
-        elif copie.count('#')==1:
-        #ADD Rd,Rn,#imm3  
-            
-
-    return assembleur_offset
-
-def add_rd_imm8(instruction):
-    
     return 1
 
-def add_rd_rn_imm3(instruction):
-    return 1
 
-def add_rd_rn_rm(instruction):
+
+
+def sub(instruction):
+
+    n=len(instruction)
+    l=''
     return 1
