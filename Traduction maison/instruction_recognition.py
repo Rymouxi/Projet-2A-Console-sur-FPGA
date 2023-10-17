914ece318c91fb54bc868b21206b011cbd1ed84f
Nyries <145461178+Nyries@users.noreply.github.com>
2023-10-17 16:29:20 +0200

    Create instruction_recognition.py

---
+++ Traduction maison/instruction_recognition.py
@@ -0,0 +1,30 @@
+def instruction_recognition(instruction):
+  """Reconnaissance des instructions données
+  """
+    n=len(instruction)
+    l=''
+    if instruction.count(':'):
+        return l
+    if instruction[0:3]=='ADD':
+        l=ADD(instruction)
+    elif instruction[0:3]=='AND ':
+        l=AND_(instruction)
+    elif instruction[0]=='B':
+        l=b_instruct(instruction)
+    elif instruction[0:3]=='CMP':
+        l=CMP(instruction)
+    elif instruction[0:3]=='EOR':
+        l=EOR(instruction)
+    elif instruction[0:3]=='LDR':
+        l=LDR(instruction)
+    elif instruction[0:3]=='LSL':
+        l=LSL(instruction)
+    elif instruction[0:3]=='MOV':
+        l=MOV(instruction)
+    elif instruction[0:3]=='STR':
+        l=STR(instruction)
+    elif instruction[0:3]=='SUB':
+        l=SUB(instruction)
+    else:
+        print("Error Syntax")
+    return l
