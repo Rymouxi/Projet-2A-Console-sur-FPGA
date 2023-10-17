2675f052c99ad115ba58158270267f4a820eeb12
Nyries <145461178+Nyries@users.noreply.github.com>
2023-10-17 16:37:16 +0200

    Update Fonctions_LCM3.py

--- Traduction maison/Fonctions_LCM3.py
+++ Traduction maison/Fonctions_LCM3.py
@@ -1,39 +1,7 @@
-from DecToBin import *
-from add_sub_mov import *
 from Register_recognition import *
-from B_instruct import*
 
 #On travaille toujours avec des chaînes de caractères
 #Les fonctions appelées renvoient des chaînes de caractère
-
-def recognition_instruction(instruction):
-    n=len(instruction)
-    l=''
-    if instruction[0:3]=='ADD':
-        l=ADD(instruction)
-    elif instruction[0:3]=='AND ':
-        l=AND_(instruction)
-    elif instruction[0]=='B':
-        l=b_instruct(instruction)
-    elif instruction[0:3]=='CMP':
-        l=CMP(instruction)
-    elif instruction[0:3]=='EOR':
-        l=EOR(instruction)
-    elif instruction[0:3]=='LDR':
-        l=LDR(instruction)
-    elif instruction[0:3]=='LSL':
-        l=LSL(instruction)
-    elif instruction[0:3]=='MOV':
-        l=MOV(instruction)
-    elif instruction[0:3]=='STR':
-        l=STR(instruction)
-    elif instruction[0:3]=='SUB':
-        l=SUB(instruction)
-    else:
-        print("Error Syntax")
-    return l
-
-
 def AND_(instruction:str):
     """Traduction de l'instruction and en langage machine de 0 et de 1\n
     L'instruction qu'elle renvoie est de type str"""
@@ -68,8 +36,8 @@ def LDR(instructions:str):
     """   
     """
     if len(register_recognition(instructions))==2:
-        machine5='0110100000'+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
-        return(machine5)
+        machine='0110100000'+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine)
     else:
         return(print("error systéme sur le LDR dans le arguments"))
     
@@ -78,17 +46,80 @@ def EOR(instructions:str):
     """
     """    
     if len(register_recognition(instructions))==2:
-        machine5='0100000001'+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
-        return(machine5)
+        machine='0100000001'+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine)
     else:
         return(print("error systéme sur le EOR dans le arguments"))
     
 def CMP(instructions:str):
-     """Traduction de l'instruction and en langage machine de 0 et de 1\n
+    """Traduction de l'instruction and en langage machine de 0 et de 1\n
     L'instruction qu'elle renvoie est de type str"""
     #CMP Rn,#imm8
     if len(register_recognition(instructions))==1:
-        machine5='00101'+(register_recognition(instructions)[0])+(htag_recognition(instructions,8))
-        return(machine5)
+        return '00101'+(register_recognition(instructions)[0])+(htag_recognition(instructions,8))
     else:
         return(print("error systéme sur le EOR dans le arguments"))
+
+
+def ADD(instructions:str):
+    """ 3 modes de fonctionnement pour la fonction ADD
+    
+    """
+    if len(register_recognition(instructions))==3:
+        machine1='0001100'+(register_recognition(instructions)[2])+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine1)
+    if len(register_recognition(instructions))==2:
+        if htag_recognition(instructions,3)==-1:
+            return(print("Tu dois mettre un nombre dans le add si 2 registre"))
+        machine2='0001110'+(htag_recognition(instructions,3))+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine2)
+    if len(register_recognition(instructions))==1:
+        if htag_recognition(instructions,8)==-1:
+            return(print("Tu dois mettre un nombre dans le add si 1 registre"))
+        machine3='00110'+(register_recognition(instructions)[0])+(htag_recognition(instructions,8))
+        return(machine3)
+    else:
+        return(print("error systéme"))
+
+
+
+
+def SUB(instructions:str):
+    """ a compléter comme le add"""
+    
+    if len(register_recognition(instructions))==3:
+        machine1='0001101'+(register_recognition(instructions)[2])+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine1)
+    if len(register_recognition(instructions))==2:
+        if htag_recognition(instructions,3)==-1:
+            return(print("Tu dois mettre un nombre dans le SUB si 2 registre"))
+        machine2='0001111'+(htag_recognition(instructions,3))+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine2)
+    if len(register_recognition(instructions))==1:
+        if htag_recognition(instructions,8)==-1:
+            return(print("Tu dois mettre un nombre dans le SUB si 1 registre"))
+        machine3='00111'+(register_recognition(instructions)[0])+(htag_recognition(instructions,8))
+        return(machine3)
+    else:
+        return(print("error systéme"))    
+
+
+
+
+
+def MOV(instructions:str):
+    """ Fonctions MOV qui à 2 modes de fonctionnement, 
+    2 registre en entrées ou 1 registre et un nombre compris entre 0 et 255
+    Notre fonctions prend en entrée une chaine de carractére qui correspond a une ligne d'instruction contenant "MOV" et renvoie l'instruction machine en bianire correspondante.
+    """
+    if len(register_recognition(instructions))==2:
+        machine1='0000000000'+(register_recognition(instructions)[1])+(register_recognition(instructions)[0])
+        return(machine1)
+    if len(register_recognition(instructions))==1:
+        if (htag_recognition(instructions,8))>255 or (htag_recognition(instructions,8))<0:
+            return(print("Dans le MOV ton chiffre et trop grand COn****d ou trop petit"))  
+                                                                
+        machine2='00100'+(register_recognition(instructions)[0])+(htag_recognition(instructions,8))
+        return(machine2)
+    
+  
