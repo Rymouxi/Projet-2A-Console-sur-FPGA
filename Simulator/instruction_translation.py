313be8059515a9602a6acf82cfb0cee4a744c06c
Nyries <145461178+Nyries@users.noreply.github.com>
2023-11-09 11:26:51 +0100

    Update instruction_translation.py

--- Simulator/instruction_translation.py
+++ Simulator/instruction_translation.py
@@ -1,6 +1,7 @@
 from instruction_recognition import*
 from treatment import*
 from virtual_register_fct import virtual_register_init
+from virtual_memory_fct import virtual_memory_init
 
 def instruction_translation(ASM:str):
     """Programme global de traduction d'un code ASM en code machine
@@ -10,25 +11,36 @@ def instruction_translation(ASM:str):
     split_instructions=saut_ligne(ASM)
     #Les lignes des instructions sont numérotées à partir de 1
     line_instruction=[i+1 for i in range(len(split_instructions))]
-    bitstream=''
-    line_pointer=1
-    line_update=[1]
+    bitstream=[]
     register_update=[]
+    line_pointer=0
+    line_update=[0]
+    memory_update=[]
     error=[]
     virtual_register_init()
+    virtual_memory_init()
     
     #Réalisation du bitstream
     i=0
     for instruction in split_instructions:
-        bitstream+=instruction_recognition(instruction,i,split_instructions,simu='OFF')
+        bitstream.append(instruction_recognition(instruction,i,split_instructions,simu='OFF'))
         i+=1
     
     #Réalisation de la simulation
-    for instruction in split_instructions:
-        register_update_instruction,line_pointer,error_instruction=instruction_recognition(instruction,line_pointer,split_instructions,simu='ON')
+    split_instructions_with_END=split_instructions+["END"]
+    j=0
+    while split_instructions_with_END[line_pointer]!="END":
+        instruction=split_instructions_with_END[line_pointer]
+        register_update_instruction,line_pointer,memory_update_instruction,error_instruction=instruction_recognition(instruction,line_pointer,split_instructions,simu='ON')
         register_update.append(register_update_instruction)
         line_update.append(line_pointer)
-        error+=error_instruction
-    return split_instructions,line_instruction,bitstream,register_update,line_update,error
+        memory_update.append(memory_update_instruction)
+        error=error+error_instruction
+        j+=1
+        if j>1000:
+            error.append("There's an infinite loop at line "+str(line_pointer))
+            error.append(line_pointer)
+            break
+    return split_instructions,line_instruction,bitstream,register_update,line_update,memory_update,error
 
 
