bf4024a93119494bc854915f44d65e67dba8d925
Nyries <145461178+Nyries@users.noreply.github.com>
2023-11-14 14:22:48 +0100

    Update virtual_memory_fct.py

--- Simulator/virtual_memory_fct.py
+++ Simulator/virtual_memory_fct.py
@@ -1,25 +1,27 @@
+"""virtual_memory est une liste contenant les adresses et les valeurs de ces adresses\n
+Ces valeurs sont en décimal\n
+virtual_memory est de la forme [adresse1,valeur1,adresse2,valeur2...]"""
+
 virtual_memory=[]
 
+
 def virtual_memory_init():
-    for i in range(0,50,4):
+    for i in range(0,50):
         global virtual_memory
-        virtual_memory.append([hex(i),'0x00000000'])
+        virtual_memory.append(i)
+        virtual_memory.append(0)
 
 
 def virtual_memory_write(address,value):
     """Écrit dans la mémoire value à address\n
-    Sachant que ces deux arguments doivent être de la forme 0x00000000 
+    Sachant que ces deux arguments doivent être de type int
     """
-    global virtual_memory
-    addresses=[ad[0] for ad in virtual_memory]    
-    new_address=hex(int(address,16)+4-int(address,16)%4) 
-    virtual_memory[addresses.index(address)][1]=value
+    global virtual_memory  
+    virtual_memory[2*i+1]=value
 
 def virtual_memory_read(address):
     """Retourne la valeur présente à address
     """
     global virtual_memory
-    addresses=[ad[0] for ad in virtual_memory]  
-    new_address=hex(int(address,16)+4-int(address,16)%4) 
-    value=virtual_memory[addresses.index(address)][1]
+    value=virtual_memory[2*i+1]
     return value
