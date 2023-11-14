ca182967aff9076ac8466a68dfabdf169743fed5
Nyries <145461178+Nyries@users.noreply.github.com>
2023-11-14 18:35:36 +0100

    Update B_instruct_bitstream.py

--- Simulator/B_instruct_bitstream.py
+++ Simulator/B_instruct_bitstream.py
@@ -16,6 +16,18 @@ def B_instruct_bitstream(instruction:str,split_instructions:list,line:int):
     elif instruction[0:3]=='BNE':
         #BNE label
         bitstream=BNE_label_bitstream(instruction,label_list,line)
+    elif instruction[0:3]=='BGE':
+        #BNE label
+        bitstream=BGE_label_bitstream(instruction,label_list,line)
+    elif instruction[0:3]=='BLT':
+        #BNE label
+        bitstream=BLT_label_bitstream(instruction,label_list,line)
+    elif instruction[0:3]=='BGT':
+        #BNE label
+        bitstream=BGT_label_bitstream(instruction,label_list,line)
+    elif instruction[0:3]=='BLE':
+        #BNE label
+        bitstream=BLE_label_bitstream(instruction,label_list,line)
     elif instruction[0:2]=='B ':
         #B label
         bitstream=B_label_bitstream(instruction,label_list,line)
@@ -56,6 +68,45 @@ def BEQ_label_bitstream(instruction:str,label_list,code_ASM,line:int):
 
     return bitstream
 
+def BGE_label_bitstream(instruction:str,label_list,line:int):
+    """Traduction de l'instruction BGE label
+    """
+
+    n=len(instruction)
+    jumpBin=jump_length_bitstream(instruction[4:n+1],label_list,line,7)
+    bitstream= '11011010'+jumpBin
+
+    return bitstream
+
+def BLT_label_bitstream(instruction:str,label_list,line:int):
+    """Traduction de l'instruction BLT label
+    """
+
+    n=len(instruction)
+    jumpBin=jump_length_bitstream(instruction[4:n+1],label_list,line,7)
+    bitstream= '11011011'+jumpBin
+
+    return bitstream
+
+def BGT_label_bitstream(instruction:str,label_list,line:int):
+    """Traduction de l'instruction BGT label
+    """
+
+    n=len(instruction)
+    jumpBin=jump_length_bitstream(instruction[4:n+1],label_list,line,7)
+    bitstream= '11011100'+jumpBin
+
+    return bitstream
+
+def BLE_label_bitstream(instruction:str,label_list,line:int):
+    """Traduction de l'instruction BLE label
+    """
+
+    n=len(instruction)
+    jumpBin=jump_length_bitstream(instruction[4:n+1],label_list,line,7)
+    bitstream= '11011101'+jumpBin
+
+    return bitstream
 
 def jump_length_bitstream(label:str,label_list:list,line:int,size:int):
     """Cette fonction prend l'instruction sans l'embranchement même conditionnelle\n
