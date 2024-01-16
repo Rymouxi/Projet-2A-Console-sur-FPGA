76f1e8fddc15c5ec83b15527712ea82b287916e6
Yumeng <109465802+MidsummerYU@users.noreply.github.com>
2024-01-16 17:30:01 +0100

    Update Fonction de multiplication.asm
    multiplication update

--- Code_Jeu_ASM/FunctionBasics/Fonction de multiplication.asm
+++ Code_Jeu_ASM/FunctionBasics/Fonction de multiplication.asm
@@ -1,23 +1,21 @@
-;Fonction de multiplication
-;Résultat = R1 * R2
-
+; Fonction de multiplication
+; Résultat = R1 * R2
 MUL:
-    MOV R0, #0      ; Initialiser le résultat à zéro
-    MOV R3, #0      ; Initialiser un registre temporaire à zéro
+MOV R0, #0      		; Initialiser le résultat à zéro
 
-ADD R3, R0, R1      ; Ajouter le multiplicand au résultat temporaire
-CMP R3, R0          ; Vérifier s'il y a eu un dépassement (overflow)
-BNE MUL_DONE        ; Si oui, sortir de la boucle (si R1 = R3 = 0 )
+    	MUL_LOOP:
+       		 CMP R2, #0          	; Vérifier si le multiplicateur R1 est nul
+       		 BEQ MUL_DONE        	; Si c'est le cas, sortir de la boucle
 
-MUL_LOOP:
-    CMP R2, #0      ; Vérifier si le multiplicateur est nul
-    BEQ MUL_DONE    ; Si c'est le cas, sortir de la boucle
+CMP R1, #0         	; Vérifier si le multiplicand est nul
+       		BEQ MUL_DONE        	; Si c'est le cas, sortir de la boucle
 
-    ADD R0, R0, R1  ; Ajouter le multiplicand au résultat (R0 = R1)
-    SUB R2, R2, #1  ; Décrémenter le multiplicateur
-    B MUL_LOOP      ; Répéter la boucle
+ADD R0, R0, R1      	; R0 = R0 + R1
+       		SUB  R2, R2, #1     	; Décrémenter le multiplicateur   
+CMP R2, #0      
+BGE MUL_LOOP        	; Répéter la boucle si le multiplicateur est >= 0
 
-MUL_DONE:
-    ; le résultat est dans le registre R0
-    BX LR           ; Retourner de la fonction
+    	MUL_DONE:
+        	; À ce stade, le résultat est dans le registre R0
+        	BX LR              			 ; Retourner de la fonction
 
