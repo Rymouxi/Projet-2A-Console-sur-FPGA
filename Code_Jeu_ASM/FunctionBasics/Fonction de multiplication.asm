9f896804cd15b738e9bfc21d285d6079641e6801
GlinglinMerguez <146117067+GlinglinMerguez@users.noreply.github.com>
2024-03-29 18:03:34 +0100

    Update Fonction de multiplication.asm

--- Code_Jeu_ASM/FunctionBasics/Fonction de multiplication.asm
+++ Code_Jeu_ASM/FunctionBasics/Fonction de multiplication.asm
@@ -1,24 +1,19 @@
-; Fonction de multiplication
-; Résultat R0 = R1 * R2
-
-; Exemple
-
-;MOV R1, #8
-;MOV R2, #7
+; Multiplication function
+; Returns R0 = R1 * R2
 
 MULTIPLICATION:
-MOV R0, #0		; Initialiser le résultat à zéro
+MOV R0, #0		; Initialize the result at zero
 
 MUL_LOOP:
-CMP R2, #0		; Vérifier si le multiplicateur R1 est nul
-BEQ MUL_DONE		; Si c'est le cas, sortir de la boucle
+CMP R2, #0		; Check if R2 factor is nil
+BEQ MUL_DONE		; If yes, break
 
-CMP R1, #0		; Vérifier si le multiplicand est nul
-BEQ MUL_DONE		; Si c'est le cas, sortir de la boucle
+CMP R1, #0		; Check if R1 factor is nil
+BEQ MUL_DONE		; If yes, break
 
 ADD R0, R0, R1		; R0 = R0 + R1
-SUB  R2, R2, #1		; Décrémenter le multiplicateur   
+SUB  R2, R2, #1		; Decrement the R2 factor
 CMP R2, #0      
-BGE MUL_LOOP		; Répéter la boucle si le multiplicateur est >= 0
+BGE MUL_LOOP		; Repeat if R2 >= 0
 
 MUL_DONE:
