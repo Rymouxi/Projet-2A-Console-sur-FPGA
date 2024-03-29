5099e4dc50d46e25c994dd2468c3d25d9f37804b
GlinglinMerguez <146117067+GlinglinMerguez@users.noreply.github.com>
2024-03-29 18:29:09 +0100

    Update PUSH.asm

--- Code_Jeu_ASM/FunctionBasics/PUSH.asm
+++ Code_Jeu_ASM/FunctionBasics/PUSH.asm
@@ -1,13 +1,20 @@
-;PUSH (R2) 
-MOV R0, #0x80000100
+; PUSH (R3)
+ 
+; R0 <- 0x20000004 (adress pointer)
+; R2 <- 0x20000100 (size of the pile)
+MOV R0, #1
+LSL R0, R0, #29
+ADD R2, R2, R0
+ADD R0, #4
+ADD R2, #0x80
+ADD R2, #0x80
 
-DECALAGE
-     LDR R1, [R0]
-     SUB  R0, R0, #4
-     STR R1, [R0]
-     ADD R0, R0, #8
-     CMP R0, #0x80000100
-     BNE DECALAGE
+SHIFT_ALL:
+	LDR R1, [R0]
+	SUB  R0, R0, #4
+	STR R1, [R0]
+	ADD R0, #8
+	CMP R0, R2
+	BNE SHIFT_ALL
 
-STR R2, [R0]
-
+STR R3, [R0]
